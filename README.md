16fbfbb6bebb395e6eb2e38b0f44211cd6f27566
Mael Ortie <mael.ortie@eliberty.fr>
2022-04-20 15:36:42 +0200

    add auto trace

---
+++ README.md
@@ -0,0 +1,7 @@
+# Playwright Test
+
+## Traces
+
+View traces : `npx playwright show-trace ./tests/results/{test-folder}/trace.zip`
+
+You could also use https://trace.playwright.dev/
--- playwright.config.ts
+++ playwright.config.ts
@@ -37,8 +37,10 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
+
+  outputDir: 'tests/results',
 
   /* Configure projects for major browsers */
   projects: [
